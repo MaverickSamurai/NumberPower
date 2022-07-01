260bd113bd9914483acb04580c5015eed693e13b
MaverickSamurai <mirsamirba@code.edu.az>
2022-07-01 14:29:32 +0400

    NumbersPower

---
+++ ExPower/ExPower/Helpers/NumPower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExPower.Extension
+{
+    public static class NumPower
+    {
+        
+        public static double Power(this int num, double power)
+            
+        {
+
+            return Math.Pow(num, power);
+        }
+
+       
+    } 
+}
---
+++ ExPower/ExPower/Program.cs
@@ -0,0 +1,17 @@
+﻿using ExPower.Extension;
+using System;
+
+namespace ExPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           var result = NumPower.Power(2,5);
+            Console.WriteLine(result);
+            
+            
+        }
+    }
+}
